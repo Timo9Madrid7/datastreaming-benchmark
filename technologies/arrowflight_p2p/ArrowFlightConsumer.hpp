b38490b7d9d2ee7ac7eef91aaf99c675a9604da8
Timo <631044206@qq.com>
2025-12-30 12:35:24 +0100

    Add FIXME for hardcoded thread pool size
    A FIXME comment was added to indicate that the number of threads in the thread pool is currently hardcoded and should be made configurable.

--- technologies/arrowflight_p2p/ArrowFlightConsumer.hpp
+++ technologies/arrowflight_p2p/ArrowFlightConsumer.hpp
@@ -27,6 +27,7 @@ class ArrowFlightConsumer : public IConsumer {
 	std::vector<std::pair<std::string, std::string>> ticket_publisher_pairs_;
 	int publisher_port_;
 
+	//FIXME: hardcoded number of threads, make it configurable
 	BS::pause_thread_pool thread_pool_{4};
 
 	void consume_from_publisher_(const std::string &endpoint,
