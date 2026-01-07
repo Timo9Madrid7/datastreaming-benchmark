9195770ff9577d9e1195b4cf8a6353ffaf8f1487
Timo <631044206@qq.com>
2026-01-07 14:21:57 +0100

    Fix kafka-storage.sh path in entrypoint script
    Updated the path to kafka-storage.sh in entrypoint_pub.sh to use the absolute path (/opt/kafka/bin/), ensuring the script works regardless of the current working directory.

--- technologies/kafka_p2p/entrypoint_pub.sh
+++ technologies/kafka_p2p/entrypoint_pub.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 set -e
 
-KAFKA_CLUSTER_ID="$(bin/kafka-storage.sh random-uuid)" && \
+KAFKA_CLUSTER_ID="$(/opt/kafka/bin/kafka-storage.sh random-uuid)" && \
 /opt/kafka/bin/kafka-storage.sh format -t "$KAFKA_CLUSTER_ID" -c /app/technologies/kafka_p2p/server.properties \
 && /opt/kafka/bin/kafka-server-start.sh /app/technologies/kafka_p2p/server.properties &
 until ss -ltn | grep -Eq ':9092[[:space:]]'; do
